c71fd65a06a0b0459abc4cefd035878514b7058e
EvanSteedley <43506468+EvanSteedley@users.noreply.github.com>
2021-09-01 18:49:50 -0400

    Recycler View of Cats can now be updated
    When the "add cat" button is pressed, a new instance of cat10.jpg is added, and the RecyclerView is notified of the change so that it can display the Cat correctly.  This will be useful later for dynamically updating the Recycler for 'infinite scrolling'.

--- app/src/main/java/com/example/cat_gallery/MainActivity.kt
+++ app/src/main/java/com/example/cat_gallery/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.cat_gallery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cat_gallery.adapter.CatAdapter
 import com.example.cat_gallery.data.Datasource
+import com.example.cat_gallery.model.Cat
 import javax.sql.DataSource
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +19,12 @@ class MainActivity : AppCompatActivity() {
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
         recyclerView.adapter = CatAdapter(this, myDataset)
         recyclerView.layoutManager = GridLayoutManager(applicationContext, 2)
-        recyclerView.setHasFixedSize(true)
+        recyclerView.setHasFixedSize(false)
+        val catButton = findViewById<Button>(R.id.catButton)
+        catButton.setOnClickListener{
+            myDataset.add(Cat(R.drawable.cat10))
+            catButton.text = myDataset.size.toString()
+            (recyclerView.adapter as CatAdapter).notifyItemChanged(myDataset.size - 1)
+        }
     }
 }
--- app/src/main/java/com/example/cat_gallery/data/Datasource.kt
+++ app/src/main/java/com/example/cat_gallery/data/Datasource.kt
@@ -4,8 +4,8 @@ import com.example.cat_gallery.R
 import com.example.cat_gallery.model.Cat
 
 class Datasource {
-fun loadCats(): List<Cat> {
-    return listOf<Cat>(
+fun loadCats(): MutableList<Cat> {
+    return mutableListOf<Cat>(
         Cat(R.drawable.cat1),
         Cat(R.drawable.cat2),
         Cat(R.drawable.cat3),
@@ -14,8 +14,7 @@ fun loadCats(): List<Cat> {
         Cat(R.drawable.cat6),
         Cat(R.drawable.cat7),
         Cat(R.drawable.cat8),
-        Cat(R.drawable.cat9),
-        Cat(R.drawable.cat10)
+        Cat(R.drawable.cat9)
     )
 }
 }
